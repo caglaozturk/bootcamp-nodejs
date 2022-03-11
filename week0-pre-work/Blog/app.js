b085ba9297708f574d678e2f5d6c786134c03e06
Cagla Ozturk <ozturkcagla041@gmail.com>
2022-03-11 12:37:41 +0300

    Static files uploaded

--- week0-pre-work/Blog/app.js
+++ week0-pre-work/Blog/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
